5682860090496378f74324ee7aa1b4bcabbf9cfb
Syakir Arif <muhsyakirarif12@gmail.com>
2023-12-05 10:21:05 +0700

    Refactored LoginActivity.kt

--- features/login/src/main/java/com/aniiki/features/login/LoginActivity.kt
+++ features/login/src/main/java/com/aniiki/features/login/LoginActivity.kt
@@ -2,24 +2,11 @@ package com.aniiki.features.login
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginActivity : ComponentActivity() {
-
-    private val viewModel: LoginVM by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        fetchData()
-//        setContent {
-//            BidayahTheme {
-//                fetchData()
-//            }
-//        }
-    }
-
-    private fun fetchData() {
     }
 }
