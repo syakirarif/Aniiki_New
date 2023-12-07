5ee4caa2d59850d77a4ab4b06bb84aa66472cb29
Syakir Arif <muhsyakirarif12@gmail.com>
2023-12-07 09:18:47 +0700

    Commented LoadingScreen composable

--- features/login/src/main/java/com/aniiki/features/login/LoginScreen.kt
+++ features/login/src/main/java/com/aniiki/features/login/LoginScreen.kt
@@ -2,24 +2,19 @@ package com.aniiki.features.login
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -158,18 +153,18 @@ import com.syakirarif.aniiki.compose.custom.StaggeredVerticalGrid
 //    }
 //}
 
-@Composable
-fun LoadingScreen(
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        CircularProgressIndicator(color = Color.Blue)
-    }
-}
+//@Composable
+//fun LoadingScreen(
+//    modifier: Modifier = Modifier
+//) {
+//    Column(
+//        modifier = modifier.fillMaxSize(),
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//        CircularProgressIndicator(color = Color.Blue)
+//    }
+//}
 
 @Composable
 fun MainAnimeLayout(
