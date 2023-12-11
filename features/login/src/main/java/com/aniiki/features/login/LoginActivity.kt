3b4b3292744dc9deb498b46a752ad0b78c2aa1e2
Syakir Arif <muhsyakirarif12@gmail.com>
2023-12-11 10:18:45 +0700

    Removed @AndroidEntryPoint in LoginActivity.kt

--- features/login/src/main/java/com/aniiki/features/login/LoginActivity.kt
+++ features/login/src/main/java/com/aniiki/features/login/LoginActivity.kt
@@ -2,9 +2,7 @@ package com.aniiki.features.login
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
