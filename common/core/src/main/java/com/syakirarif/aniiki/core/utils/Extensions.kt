b1bb70eae21670a54270d89fa851d80069aa026b
Syakir Arif <muhsyakirarif12@gmail.com>
2023-12-14 10:04:17 +0700

    Commented hideKeyboard()

--- common/core/src/main/java/com/syakirarif/aniiki/core/utils/Extensions.kt
+++ common/core/src/main/java/com/syakirarif/aniiki/core/utils/Extensions.kt
@@ -82,9 +82,9 @@ import java.util.Locale
 //    view?.let { activity?.hideKeyboard(it) }
 //}
 
-fun Activity.hideKeyboard() {
-    hideKeyboard(currentFocus ?: View(this))
-}
+//fun Activity.hideKeyboard() {
+//    hideKeyboard(currentFocus ?: View(this))
+//}
 
 fun Context.hideKeyboard(view: View) {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
