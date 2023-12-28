1becf1778fccfb67b5f13ace2fe0081e50b6f711
Syakir Arif <muhsyakirarif12@gmail.com>
2023-12-28 13:11:40 +0700

    Updated Kotlin version to 1.9.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("com.android.application") version "8.1.2" apply false
 //    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+//    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
 //    id("com.google.gms.google-services") version "4.3.15" apply false
