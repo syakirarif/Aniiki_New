edde496058a0cf47c16957e27aba429807e960e9
Syakir Arif <muhsyakirarif12@gmail.com>
2023-12-28 13:19:13 +0700

    Updated KSP version to 1.9.21-1.0.16

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 //    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
-    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+    id("com.google.devtools.ksp") version "1.9.21-1.0.16" apply false
 //    id("com.google.gms.google-services") version "4.3.15" apply false
 //    id("com.google.firebase.crashlytics") version "2.9.9" apply false
     id("com.savvasdalkitsis.module-dependency-graph") version "0.10" apply false
